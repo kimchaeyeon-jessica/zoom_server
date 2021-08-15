8d242812048a740633d9e84a53c3e9fc43f9f7a0
kimchaeyeon <jessicakcy0501@gmail.com>
2021-08-16 06:18:26 +0900

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import express from 'express';
-import { createServer, Server } from 'http';
+import https from 'https';
+import fs from 'fs';
 import bodyParser from 'body-parser';
 
+import * as path from 'path';
 import controller from './controller';
+import { initWebSocket } from './middleware/socket';
+import config from './config';
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, config.sslKey), 'utf-8'),
+  cert: fs.readFileSync(path.join(__dirname, config.sslCrt), 'utf-8'),
+};
 
 const app = express();
+const httpsServer = https.createServer(options, app);
 
 app.use(bodyParser.json());
 app.use(controller);
 
-const server = createServer(app);
-server.listen(process.env.PORT || 5000);
+initWebSocket(httpsServer);
+
+httpsServer.listen(process.env.PORT || 5000, () => {
+  console.log('server initialized');
+});
